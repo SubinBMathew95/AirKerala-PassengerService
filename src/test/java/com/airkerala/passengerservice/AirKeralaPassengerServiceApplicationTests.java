94ed309e0955bcf43e9fa77bece6cfe39b68b072
SubinBMathew95 <130916190+SubinBMathew95@users.noreply.github.com>
2023-04-16 22:10:42 +0530

    Initial Commit

---
+++ src/test/java/com/airkerala/passengerservice/AirKeralaPassengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airkerala.passengerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirKeralaPassengerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
